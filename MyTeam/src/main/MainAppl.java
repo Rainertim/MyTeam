a397d79d57b8a36088ccb0a9ac72e1f5eb401915
Rainertim <77675660+Rainertim@users.noreply.github.com>
2021-06-21 15:23:19 +0200

    + better formating of output

--- MyTeam/src/main/MainAppl.java
+++ MyTeam/src/main/MainAppl.java
@@ -9,7 +9,7 @@ public class MainAppl {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("== " + player);
 		player = new Player(3, "John", "Lemmon");
-		System.out.println("== " + player);
+		System.out.println("=== " + player);
 	}
 
 }
